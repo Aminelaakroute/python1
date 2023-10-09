911a04d9c96b8ac636533744127513eb6b84e95d
AmineLke <amine.laakroute@etu.uae.ac.ma>
2023-10-09 22:18:26 +0100

    1

--- ex40.py
+++ ex40.py
@@ -8,10 +8,10 @@ noms_de_famille = ['Smith', 'Johnson', 'Williams', 'Jones', 'Brown', 'Davis', 'M
 def generer_nom_aleatoire():
     prenom = random.choice(prenoms)
     nom_de_famille = random.choice(noms_de_famille)
-    nom_complet = f"{prenom} {nom_de_famille}"
+    nom_complet = f"{prenom}{nom_de_famille}"
     return nom_complet
 
 # Exemple d'utilisation
-for _ in range(5):
+for _ in range(15):
     nom_aleatoire = generer_nom_aleatoire()
     print(nom_aleatoire)
